22668f220df9067b124ba80895fac4df741d0971
William Nkosi <william.dev.acc@gmail.com>
2020-11-27 00:14:36 -0600

    setup the RequestAsync in the GitHubService class, app current broken

--- app/src/main/java/nkosi/william/gmtechassignment/GitHubService.java
+++ app/src/main/java/nkosi/william/gmtechassignment/GitHubService.java
@@ -1,5 +1,9 @@
 package nkosi.william.gmtechassignment;
 
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 import nkosi.william.gmtechassignment.models.Commit;
@@ -8,6 +12,30 @@ public class GitHubService {
     ArrayList<Commit> listOfCommits = new ArrayList<Commit>();
 
     public GitHubService() {
+        new RequestAsync().execute();
+    }
+
+    private ArrayList<Commit> createListOfCommits(JSONObject json) {
+        
+    }
+
+    private class RequestAsync extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try{
+                //Get Request
+                String json = RequestHandler.httpSendGet("https://api.github.com/repos/williamnkosi/GM_Tech_Assignment/commits");
+                if(json != null){
+                    JSONObject jsonObject = new JSONObject(json);
+                    listOfCommits = createListOfCommits(jsonObject);
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return  null;
+        }
 
     }
 }
