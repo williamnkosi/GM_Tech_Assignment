d1956b01a8a2dfc5f94b6d87c638e65b7a7fbdef
William Nkosi <william.dev.acc@gmail.com>
2020-11-26 20:48:00 -0600

    Created the commit class

--- app/src/main/java/nkosi/william/gmtechassignment/models/Commit.java
+++ app/src/main/java/nkosi/william/gmtechassignment/models/Commit.java
@@ -1,4 +1,13 @@
 package nkosi.william.gmtechassignment.models;
 
 public class Commit {
+    String Author;
+    String commitHash;
+    String commitMessage;
+
+    public Commit(String author, String commitHash, String commitMessage) {
+        this.Author = author;
+        this.commitHash = commitHash;
+        this.commitMessage = commitMessage;
+    }
 }
